396a611a0f73e00fea44dbc5b1136d76c40c6c95
pravn27 <praveen.sri27@gmail.com>
2020-10-31 08:05:11 +0530

    Updated search page

--- src/pages/SearchPage/SearchPage.js
+++ src/pages/SearchPage/SearchPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const SearchPage = () => {
-  return <div>SearchPage</div>;
+  return <div>SearchPage , video list on search text </div>;
 };
 
 export default SearchPage;
