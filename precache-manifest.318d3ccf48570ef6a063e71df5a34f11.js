2fd0d1de7b7ba139987d0519cd1efbbdbdef283b
pravn27 <praveen.sri27@gmail.com>
2020-11-01 09:58:27 +0530

    Updates

---
+++ precache-manifest.318d3ccf48570ef6a063e71df5a34f11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5ce6a873fc9399f251d39652aae97f2",
+    "url": "/youtubeClone/index.html"
+  },
+  {
+    "revision": "d617b7754bffa4d357fc",
+    "url": "/youtubeClone/static/css/main.40b29358.chunk.css"
+  },
+  {
+    "revision": "6e976489611476f4317e",
+    "url": "/youtubeClone/static/js/2.f3f49a43.chunk.js"
+  },
+  {
+    "revision": "f3a6a51dd260c4f924bf796e7b33f546",
+    "url": "/youtubeClone/static/js/2.f3f49a43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d617b7754bffa4d357fc",
+    "url": "/youtubeClone/static/js/main.148d551c.chunk.js"
+  },
+  {
+    "revision": "b243e890a82f4a5b1f4e",
+    "url": "/youtubeClone/static/js/runtime-main.d38884ed.js"
+  }
+]);
